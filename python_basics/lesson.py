e414177f269bbd34df7e3ff398bc7600c1ba9543
Iniguez <ciniguez2@cps.k12.il.us>
2024-09-03 12:29:20 -0500

    first commit

---
+++ python_basics/lesson.py
@@ -0,0 +1,29 @@
+# this is the print statement
+#print statement allows us to print out a statement
+#comments are not executed by the computer
+#comments are used to help us understand the code
+# print ("Hello my name is Cedahlia Iniguez")
+# print("I am a student at John Hancock")
+# print("I am learning python")
+#create input for the user
+name = input ("what is your name?")
+print ("hello", name)
+#input allows us to take input from the user
+#ask the user for their age
+#print out the age
+#ask the user for their favorite color
+#print out the color
+#ask the user for their favorite food
+#print out the favorite food
+age = input("what is your age?")
+print (age)
+color= input ("what is your favorite color?")
+print (color)
+food= input ("what is your favorite food?")
+print (food)
+#concantenation is the process of combining string with
+#variables
+# print ("you are", age, "years old")
+favorite_animal = input ("what is your favorite animal?")
+print("your favorite is a "+ favorite_animal)
+# the plus sign is used to concantenation
