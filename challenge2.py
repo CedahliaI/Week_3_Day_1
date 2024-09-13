361ed9a8ec94300d7b3b13e7e41247aacea1b36c
Iniguez <ciniguez2@cps.k12.il.us>
2024-09-13 12:28:52 -0500

    Valid Variable

---
+++ challenge2.py
@@ -0,0 +1,7 @@
+name = input("What is your name? ")
+month_sales = float(input("How much did you make in sales this month? "))
+commission = (float(month_sales) * 0.13)
+print("Welcome, " + name + ". You've made a total of " + str(round(commission)) + " dollars! ")
+
+
+
---
+++ testColab.py
@@ -0,0 +1,17 @@
+# This is just for testing
+# For collab purposes
+# Create an input for the user to input their age
+# Create an input for the user to input their name
+# Create an input for the user to input their favorite song
+
+
+age = input("how old are you?")
+print("I am" , age)
+
+
+name = input("what is your name?")
+print("My name is" , name)
+
+song = input("What is your favorite song?")
+print("My favorite song is" , song)
+
